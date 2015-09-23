4a0edcc4575267a339f16b68fe5af6ced3de0dcb
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2015-09-23 13:53:03 +1200

    Initial project setup.

---
+++ teapot.rb
@@ -0,0 +1,54 @@
+# Teapot configuration generated at 2015-09-23 13:36:41 +1200
+
+required_version "1.0.0"
+
+# Build Targets
+
+define_target "robot-control" do |target|
+	target.build do
+		source_root = target.package.path + 'source'
+		
+		copy headers: source_root.glob('RobotControl/**/*.{h,hpp}')
+		
+		build static_library: "robot-control", source_files: source_root.glob('RobotControl/**/*.cpp')
+	end
+	
+	target.depends "Build/Files"
+	target.depends "Build/Clang"
+	
+	target.depends :platform
+	target.depends "Language/C++11"
+	
+	target.provides "Executable/RobotControl"
+end
+
+define_target "robot-control-tests" do |target|
+	target.build do
+		test_root = target.package.path + 'test'
+		
+		run tests: "RobotControl", source_files: test_root.glob('RobotControl/**/*.cpp')
+	end
+	
+	target.depends "Build/Clang"
+	
+	target.depends :platform
+	target.depends "Language/C++11"
+	target.depends "Library/UnitTest"
+	
+	target.provides "Test/RobotControl"
+end
+
+# Configurations
+
+define_configuration "robot-control" do |configuration|
+	configuration[:source] = "https://github.com/dream-framework"
+
+	configuration.require "build-files"
+	configuration.require "platforms"
+	configuration.require "variants"
+	configuration.require "language-cpp-class"
+	configuration.require "unit-test"
+	
+	configuration.require "opencv"
+end
+
